23d069822bcdb7dcd5344db658e2417fa849f1dd
Tarık <95959664+YazilimMeraki@users.noreply.github.com>
2022-02-14 17:08:34 +0300

    Base64
    Base64 Şifreleme

---
+++ Base64Sifre/Ders13_ŞifrelemeAlgoritmaları_Base64/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders13_ŞifrelemeAlgoritmaları_Base64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Şifleme için
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Base 64 8 bitlik dizileri 64 bite çevirir.
+            string anametin = textBox1.Text;
+            // Byte dizisi oluşturdul. ASCIIEndocing formatı her karakterin bilgisayar karşılığıdır.
+            // Anametni byte olarak al komutu verdik.
+            byte[] veridizimi = ASCIIEncoding.ASCII.GetBytes(anametin);
+            // Veridizimi adlı diziyi base64string formatına çevirdik.
+            string şifrelimetin = Convert.ToBase64String(veridizimi);
+            // Textbox a şifreli metni yazdırdık.
+            textBox2.Text = şifrelimetin;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Şifreyi çözmek için.
+            // Metni eşleştirdik.
+            string metinçöz = textBox2.Text;
+            // Byte dizisi oluşturduk ve base64stringten metincözü dönüştürdük.
+            byte[] veridizimcözülen = Convert.FromBase64String(metinçöz);
+            // Anametin2 yi ASSCI deb get string'e dönüştürdük.
+            string anametin2 = ASCIIEncoding.ASCII.GetString(veridizimcözülen);
+            // Metni yazdırdık.
+            textBox1.Text = anametin2;
+
+        }
+    }
+}
---
+++ Base64Sifre/Ders13_ŞifrelemeAlgoritmaları_Base64/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders13_ŞifrelemeAlgoritmaları_Base64
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
